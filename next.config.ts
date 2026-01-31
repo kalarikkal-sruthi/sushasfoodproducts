c4a441789c454c4332ca901bc2508f9dab6731b3
kalarikkal-sruthi <kalarikkalsruthi79@gmail.com>
2026-02-01 01:08:04 +0530

    added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    images: {
-    domains: ['images.unsplash.com', 'example.com'], // Add your image domains
+    // domains: ['images.unsplash.com', 'example.com'], // Add your image domains
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
